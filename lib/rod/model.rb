d31206e8d59f9bd03f27999681a8f8d750a70c02
Aleksander Pohl <apohllo@o2.pl>
2010-06-21 19:47:24 +0200

    - Raise exception if some of the refernced objects were not stored in the DB

--- lib/rod/model.rb
+++ lib/rod/model.rb
@@ -233,6 +233,9 @@ module Rod
       if @readonly
         loader_class.close(@handler, nil)
       else
+        unless referenced_objects.select {|k, v| not v.empty?}.size == 0
+          raise "Not all associations have been stored: #{referenced_objects}"
+        end
         exporter_class.close(@handler, self.subclasses)
       end
       @handler = nil
