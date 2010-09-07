6dc70cbff1ffe377f33053d854b826e958af7b59
Aleksander Pohl <apohllo@o2.pl>
2010-09-08 00:30:30 +0200

    - Fix RubyInline / ActiveSupport incompatibility

--- lib/rod.rb
+++ lib/rod.rb
@@ -3,5 +3,10 @@ require 'english/inflect'
 require 'weak_hash'
 require 'active_model'
 
+alias :oldrequire :require
+def require(*args)
+  oldrequire(*args)
+end
+
 files = Dir.glob(File.join(File.dirname(__FILE__), 'rod/**.rb'))
 files.each{ |f| require f }
