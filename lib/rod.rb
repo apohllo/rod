55e3051274c6357151c1da9bf486f108f8c1f2ed
Aleksander Pohl <apohllo@o2.pl>
2011-05-11 12:00:01 +0200

    - Warkaround for #21: set ActiveSupport::Dependencis to use regular require

--- lib/rod.rb
+++ lib/rod.rb
@@ -2,11 +2,13 @@ require 'inline'
 require 'english/inflect'
 require 'weak_hash'
 require 'active_model'
+require 'active_support/dependencies'
 
-alias :oldrequire :require
-def require(*args)
-  oldrequire(*args)
-end
+# XXX This should be done in a different way, since a library should not
+# impose on a user of another library specific way of using it.
+# See #21
+ActiveSupport::Dependencies.mechanism = :require
+
 
 files = Dir.glob(File.join(File.dirname(__FILE__), 'rod/**.rb'))
 files.each{ |f| require f }
