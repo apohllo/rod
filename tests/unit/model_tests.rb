c069ff468b5a5e2a3b5fab05378e2c6d09e4c487
Kacper Walanus <kacper@walanus.com>
2012-01-11 19:16:06 +0100

    - #184: support for block-style database open

--- tests/unit/model_tests.rb
+++ tests/unit/model_tests.rb
@@ -32,83 +32,86 @@ module RodTest
   end
 
   class ModuleTests < Test::Unit::TestCase
-    def setup
-      Database.instance.create_database("tmp/test_stored_instances")
-    end
-
-    def teardown
-      Database.instance.close_database
+    def create_db
+      Database.create_database("tmp/test_stored_instances") do
+        yield
+      end
     end
 
     def test_reflection
       # A
+      create_db do
+        assert AStruct.property(:a1)
+        assert AStruct.property(:a1).type
+        assert AStruct.property(:a1).type == :integer
 
-      assert AStruct.property(:a1)
-      assert AStruct.property(:a1).type
-      assert AStruct.property(:a1).type == :integer
+        assert AStruct.property(:a2)
+        assert AStruct.property(:a2).type
+        assert AStruct.property(:a2).type == :ulong
+        assert AStruct.property(:a2).options[:index]
+        assert AStruct.property(:a2).options[:index] == :flat
 
-      assert AStruct.property(:a2)
-      assert AStruct.property(:a2).type
-      assert AStruct.property(:a2).type == :ulong
-      assert AStruct.property(:a2).options[:index]
-      assert AStruct.property(:a2).options[:index] == :flat
+        assert AStruct.property(:rod_id)
 
-      assert AStruct.property(:rod_id)
+        assert AStruct.property(:b_structs)
+        assert AStruct.property(:b_structs).options[:class_name]
+        assert AStruct.property(:b_structs).options[:class_name] == "RodTest::BStruct"
 
-      assert AStruct.property(:b_structs)
-      assert AStruct.property(:b_structs).options[:class_name]
-      assert AStruct.property(:b_structs).options[:class_name] == "RodTest::BStruct"
-
-      # B
-      assert BStruct.property(:a_struct)
-      assert BStruct.property(:rod_id)
+        # B
+        assert BStruct.property(:a_struct)
+        assert BStruct.property(:rod_id)
+      end
     end
 
     def test_instances
-      a1 = AStruct.new
-      a2 = AStruct.new
-      a3 = AStruct.new
+      create_db do
+        a1 = AStruct.new
+        a2 = AStruct.new
+        a3 = AStruct.new
 
-      b1 = BStruct.new
-      b2 = BStruct.new
+        b1 = BStruct.new
+        b2 = BStruct.new
 
-      # these are generated, non-trivial accessors, so they need to be tested
-      a1.a1 = 2
-      a1.a2 = 2000000000
-      assert a1.a1 == 2
-      assert a1.a2 == 2000000000
+        # these are generated, non-trivial accessors, so they need to be tested
+        a1.a1 = 2
+        a1.a2 = 2000000000
+        assert a1.a1 == 2
+        assert a1.a2 == 2000000000
 
-      assert a1.b_structs_count == a1.b_structs.count
-      a1.b_structs = [b1, b2]
-      assert a1.b_structs.to_a == [b1, b2]
-      assert a1.b_structs_count == a1.b_structs.count
+        assert a1.b_structs_count == a1.b_structs.count
+        a1.b_structs = [b1, b2]
+        assert a1.b_structs.to_a == [b1, b2]
+        assert a1.b_structs_count == a1.b_structs.count
 
-      b1.b = "tead-only database"
-      assert b1.b == "tead-only database"
+        b1.b = "tead-only database"
+        assert b1.b == "tead-only database"
+      end
     end
 
     def test_stored_instances
 
-      a1 = AStruct.new
-      a2 = AStruct.new
-      a3 = AStruct.new
+      create_db do
+        a1 = AStruct.new
+        a2 = AStruct.new
+        a3 = AStruct.new
 
-      b1 = BStruct.new
-      b2 = BStruct.new
+        b1 = BStruct.new
+        b2 = BStruct.new
 
-      a1.b_structs = [b1, b2]
-      a2.b_structs = [b1]
-      a3.b_structs = []
+        a1.b_structs = [b1, b2]
+        a2.b_structs = [b1]
+        a3.b_structs = []
 
-      a1.store
-      a2.store
-      a3.store
+        a1.store
+        a2.store
+        a3.store
 
-      b1.store
-      b2.store
+        b1.store
+        b2.store
 
-      assert a1.b_structs_count == a1.b_structs.count
-      #p "AStruct.count: #{AStruct.count}" <- should throw a more relevant exception
+        assert a1.b_structs_count == a1.b_structs.count
+        #p "AStruct.count: #{AStruct.count}" <- should throw a more relevant exception
+      end
     end
   end
 end
