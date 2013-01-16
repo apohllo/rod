b05325e98ca844babdb605712232aa6e885982b3
Aleksander Pohl <apohllo@o2.pl>
2013-01-16 18:37:03 +0100

    Define accessors for loading/saving props. #75

---
+++ lib/rod/database/accessor/json_accessor.rb
@@ -0,0 +1,24 @@
+require_relative 'sequence_accessor'
+require 'json'
+
+module Rod
+  module Database
+    module Accessor
+      # The accessor is used to load and save string values of a particular
+      # property from and to the database.
+      class JsonAccessor < SequenceAccessor
+        protected
+        # Dump the value before string it in the database.
+        def dump_value(value)
+          JSON::dump(value).force_encoding(ASCII_8BIT)
+        end
+
+        # Convert back the value load from the database.
+        def load_value(value)
+          value.force_encoding(ASCII_8BIT)
+          JSON::load(value)
+        end
+      end
+    end
+  end
+end
