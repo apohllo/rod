d79989439841a4d223d6889ebd793b5af99c4b33
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2013-10-27 15:24:04 +0100

    Name conversion methods are class methods now #75

--- lib/rod/model/name_conversion.rb
+++ lib/rod/model/name_conversion.rb
@@ -1,26 +1,29 @@
 module Rod
   module Model
     module NameConversion
-      # Converts the model +name+ to the C struct name.
-      def self.struct_name_for(name)
-        name.underscore.gsub(/\//,"__")
-      end
-
       # The SHA2 digest of the resource name.
       #
       # Warning: if you dynamically create classes (via Class.new)
       # this value is random, until the class is bound with a constant!
-      def name_hash
-        return @name_hash unless @name_hash.nil?
+      def self.name_hash(resource)
         # This is not used to protect any value, only to
         # distinguish names of classes. It doesn't have to be
         # very strong agains collision attacks.
-        if self.struct_name.empty?
+        if resource_name(resource).empty?
           # The resource doesn't have a name yet.
           raise AnonymousClass.new
         else
-          @name_hash = Digest::SHA2.new.hexdigest(self.struct_name).
-            to_s.to_i(16) % 2 ** 32
+          Digest::SHA2.new.hexdigest(resource_name(resource)).
+            to_s.to_i(16) % (2 ** 32)
+        end
+      end
+
+      # Converts the name of the resource to a simple form.
+      def self.resource_name(resource)
+        if resource.to_s =~ /^\#/
+          ""
+        else
+          Utils.struct_name_for(resource.to_s)
         end
       end
     end
